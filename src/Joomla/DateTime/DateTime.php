e1828fd66b3fa00eb4555c21dc35e01633891714
tomek <tomasz.hanc@gmail.com>
2014-05-25 22:34:00 +0200

    properties for datetime

--- src/Joomla/DateTime/DateTime.php
+++ src/Joomla/DateTime/DateTime.php
@@ -1,13 +1,117 @@
 <?php
 
+/**
+ * Part of the Joomla Framework Date Package
+ *
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
 namespace Joomla\DateTime;
 
-class DateTime extends \DateTime
+/**
+ * DateTime class
+ *
+ * @property-read  string   $daysinmonth   t - Number of days in the given month.
+ * @property-read  string   $dayofweek     N - ISO-8601 numeric representation of the day of the week.
+ * @property-read  string   $dayofyear     z - The day of the year (starting from 0).
+ * @property-read  boolean  $isleapyear    L - Whether it's a leap year.
+ * @property-read  string   $day           d - Day of the month, 2 digits with leading zeros.
+ * @property-read  string   $hour          H - 24-hour format of an hour with leading zeros.
+ * @property-read  string   $minute        i - Minutes with leading zeros.
+ * @property-read  string   $second        s - Seconds with leading zeros.
+ * @property-read  string   $month         m - Numeric representation of a month, with leading zeros.
+ * @property-read  string   $ordinal       S - English ordinal suffix for the day of the month, 2 characters.
+ * @property-read  string   $week          W - Numeric representation of the day of the week.
+ * @property-read  string   $year          Y - A full numeric representation of a year, 4 digits.
+ *
+ * @since  1.0
+ */
+class DateTime
 {
-    public function add($interval)
-    {
-        $newDate = new \DateTime($this->format("Y-m-d"));
-        $newDate->add($interval);
-        return $newDate;
-    }
+	/** @var \DateTime */
+	private $datetime;
+
+	/**
+	 * Constructor
+	 *
+	 * @param   string  $datetime
+	 * @param   mixed   $timezone
+	 */
+	public function __construct($datetime = 'now', \DateTimeZone $timezone = null)
+	{
+		$this->datetime = new \DateTime($datetime, $timezone);
+	}
+
+	/**
+	 * Magic method to access properties of the date given by class to the format method.
+	 *
+	 * @param   string  $name
+	 *
+	 * @return  mixed
+	 */
+	public function __get($name)
+	{
+		$value = null;
+
+		switch ($name)
+		{
+			case 'daysinmonth':
+				$value = $this->datetime->format('t');
+				break;
+
+			case 'dayofweek':
+				$value = $this->datetime->format('N');
+				break;
+
+			case 'dayofyear':
+				$value = $this->datetime->format('z');
+				break;
+
+			case 'isleapyear':
+				$value = (boolean) $this->datetime->format('L');
+				break;
+
+			case 'day':
+				$value = $this->datetime->format('d');
+				break;
+
+			case 'hour':
+				$value = $this->datetime->format('H');
+				break;
+
+			case 'minute':
+				$value = $this->datetime->format('i');
+				break;
+
+			case 'second':
+				$value = $this->datetime->format('s');
+				break;
+
+			case 'month':
+				$value = $this->datetime->format('m');
+				break;
+
+			case 'ordinal':
+				$value = $this->datetime->format('S');
+				break;
+
+			case 'week':
+				$value = $this->datetime->format('W');
+				break;
+
+			case 'year':
+				$value = $this->datetime->format('Y');
+				break;
+
+			default:
+				$trace = debug_backtrace();
+				trigger_error(
+					'Undefined property via __get(): ' . $name . ' in ' . $trace[0]['file'] . ' on line ' . $trace[0]['line'],
+					E_USER_NOTICE
+				);
+		}
+
+		return $value;
+	}
 }
--- tests/Joomla/DateTime/DateTimeTest.php
+++ tests/Joomla/DateTime/DateTimeTest.php
@@ -1,24 +1,63 @@
 <?php
+/**
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
 namespace Joomla\DateTime;
 
+/**
+ * Tests for Date class.
+ *
+ * @since  1.0
+ */
 class DateTimeTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var DateTime*/
-    private $SUT;
+	/** @var DateTime */
+	private $SUT;
 
-    protected function setUp()
-    {
-        $this->SUT = new DateTime("2014-05-22");
-    }
+	/**
+	 * Set up fixtures
+	 *
+	 * @return void
+	 */
+	protected function setUp()
+	{
+		$this->SUT = new DateTime("2014-05-22");
+	}
 
-    /** @test */
-    public function it_should_return_a_new_object_with_one_day_added()
-    {
-        $newDate = $this->SUT->add(new \DateInterval("P1D"));
+	/**
+	 * Test getters
+	 *
+	 * @test
+	 * @dataProvider seedWithPropertiesAndValues
+	 * @return void
+	 */
+	public function it_should_return_a_value_of_a_property(DateTime $datetime, $property, $propertyValue)
+	{
+		$this->assertEquals($propertyValue, $datetime->$property);
+	}
 
-        $this->assertEquals("2014-05-22", $this->SUT->format("Y-m-d"));
-        $this->assertEquals("2014-05-23", $newDate->format("Y-m-d"));
-    }
+	public function seedWithPropertiesAndValues()
+	{
+		$datetime = new DateTime("2014-05-25 12:27:39");
+		$leapyear = new DateTime("2016-05-02");
 
+		return array(
+			array($datetime, 'daysinmonth', 31),
+			array($datetime, 'dayofweek',	7),
+			array($datetime, 'dayofyear',	144),
+			array($datetime, 'isleapyear',	false),
+			array($leapyear, 'isleapyear',	true),
+			array($datetime, 'day',			25),
+			array($datetime, 'hour',		12),
+			array($datetime, 'minute',		27),
+			array($datetime, 'second',		39),
+			array($datetime, 'month',		5),
+			array($datetime, 'ordinal',		'th'),
+			array($leapyear, 'ordinal',		'nd'),
+			array($datetime, 'week',		21),
+			array($datetime, 'year',		2014),
+		);
+	}
 }
