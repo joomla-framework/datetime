f5045bba63f26783e1813d3214d19a04a8228c88
tomek <tomasz.hanc@gmail.com>
2014-08-17 13:30:05 +0200

    .travis.yml

--- tests/DateTimeTest.php
+++ tests/DateTimeTest.php
@@ -575,8 +575,9 @@ final class DateTimeTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testCanReturnATimestamp()
 	{
-		$date = new DateTime('2014-08-24 00:00:00', new \DateTimeZone('Europe/Warsaw'));
-		$this->assertEquals(1408831200, $date->getTimestamp());
+		$date = new DateTime('2014-08-24');
+		$timestamp = 1408838400 - $date->getOffset();
+		$this->assertEquals($timestamp, $date->getTimestamp());
 	}
 
 	/**
